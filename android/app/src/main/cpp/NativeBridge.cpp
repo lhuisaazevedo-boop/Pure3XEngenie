aac8ed0e53d46209fe818fcebb2ba5ab4f92805c
Pure3XDev <pure3xdev@gmail.com>
2026-07-20 09:50:41 -0300

    Pure3XEngenie v0.2.5 Alpha - Base da Fase 1 Cubo3D

---
+++ android/app/src/main/cpp/NativeBridge.cpp
@@ -0,0 +1,12 @@
+#include <jni.h>
+#include <android/log.h>
+
+#define LOG_TAG "Pure3XEngenie"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_pure3x_lhuis_MainActivity_nativeInit(JNIEnv* env, jobject thiz)
+{
+    LOGI("Pure3XEngenie inicializado.");
+}
